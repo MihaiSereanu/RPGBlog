e100eb74dcc8846e66a0babfb7470549805d2944
unknown <mihai.sereanu@gmail.com>
2020-03-24 17:18:27 +0200

    Initial setup for blog

---
+++ server.js
@@ -0,0 +1,33 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const mongoose = require("mongoose");
+const mainRouter = require("./routes/main");
+// const methodOverride = require("method-override");
+const app = express();
+
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+});
+
+const db = mongoose.connection;
+db.on("error", error => console.error(error));
+db.once("open", () => console.log("Connected to Mongoose"));
+
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+app.set("layout", "layouts/layout");
+app.use(expressLayouts);
+app.use(express.static("public"));
+
+// app.use(express.urlencoded({ extended: false }));
+// app.use(methodOverride("_method"));
+
+app.use("/", mainRouter);
+
+app.listen(process.env.PORT || 3500);
