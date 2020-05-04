01504bf2271686674e451e201c2ce62404abeae2
unknown <mihai.sereanu@gmail.com>
2020-05-04 16:21:56 +0300

    Refactored users/register.

--- routes/users.js
+++ routes/users.js
@@ -29,66 +29,39 @@ router.get("/register", checkNotAuthenticated, (request, response) => {
 });
 
 // Register Handle
-router.post("/register", checkNotAuthenticated, (request, response) => {
-  const { username, email, password, password2 } = request.body;
+router.post("/register", checkNotAuthenticated, async (request, response) => {
+  const { username, email, password } = request.body;
   let errors = [];
-
   if (!username || !email || !password) {
     errors.push({ msg: "Please enter all fields" });
   }
-
-  // if (password != password2) {
-  //   errors.push({ msg: "Passwords do not match" });
-  // }
 
   if (password.length < 6) {
     errors.push({ msg: "Password must be at least 6 characters" });
   }
 
   if (errors.length > 0) {
-    response.render("users/register", {
-      errors,
-      username,
-      email,
-      password,
-      // password2,
-    });
+    response.render("users/register", { errors, username, email, password });
   } else {
-    User.findOne({ email: email }).then((user) => {
+    try {
+      let user = await User.findOne({ email: email });
       if (user) {
         errors.push({ msg: "Email already exists" });
-        response.render("users/register", {
-          errors,
-          username,
-          email,
-          password,
-          // password2,
-        });
-      } else {
-        const newUser = new User({
-          username,
-          email,
-          password,
-        });
-
-        bcrypt.genSalt(10, (error, salt) => {
-          bcrypt.hash(newUser.password, salt, (error, hash) => {
-            if (error) throw error;
-            newUser.password = hash;
-            newUser
-              .save()
-              .then((user) => {
-                request.flash(
-                  "success_msg",
-                  "You are now registered and can log in"
-                );
-                response.redirect("/users/login");
-              })
-              .catch((error) => console.log(error));
-          });
-        });
+        response.render("users/register", { errors, username, password });
       }
-    });
+      let newUser = new User({ username, email, password });
+      bcrypt.genSalt(10, (error, salt) => {
+        bcrypt.hash(newUser.password, salt, async (error, hash) => {
+          if (error) throw error;
+          newUser.password = hash;
+          newUser = await newUser.save();
+          response.redirect("/users/login");
+        });
+      });
+    } catch (error) {
+      console.log(error);
+      response.render("users/register", { errors, username, email, password });
+    }
   }
 });
 
